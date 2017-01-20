3474f13170e27cd164515f86ff49b7b05eecee54
KazuhisaTaniguchi <jjzoow96@gmail.com>
2017-01-20 13:41:32 +0900

    init from 15

---
+++ src/posts/api/pagination.py
@@ -0,0 +1,14 @@
+from rest_framework.pagination import (
+    LimitOffsetPagination,
+    PageNumberPagination
+)
+
+
+class PostLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 1
+    max_limit = 10
+
+
+class PostPageNumberPagination(PageNumberPagination):
+    # １ページに何件表示するか
+    page_size = 2
---
+++ src/posts/api/views.py
@@ -0,0 +1,85 @@
+from django.db.models import Q
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter
+)
+
+
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView
+)
+
+from rest_framework.permissions import (
+    # AllowAny,
+    IsAuthenticated,
+    # IsAdminUser,
+    IsAuthenticatedOrReadOnly
+)
+
+from .pagination import PostLimitOffsetPagination, PostPageNumberPagination
+from .permissions import IsOwnerOrReadOnly
+
+from posts.models import Post
+from .serializers import (
+    PostListSerializer,
+    PostDetailSerializer,
+    PostCreateSerializer
+)
+
+
+class PostCreateAPIView(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostCreateSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = "slug"
+    # lookup_url_kwarg = "slug"
+
+
+class PostUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = "slug"
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostDeleteAPIView(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = "slug"
+
+
+class PostListAPIView(ListAPIView):
+    serializer_class = PostListSerializer
+    # filterとソートする処理 search と orderingでGETする
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'content', 'user__first_name']
+    # ページネーションの処理
+    pagination_class = PostPageNumberPagination
+
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Post.objects.all()
+        # こっちはqでfilterをかける
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+            ).distinct()
+        return queryset_list
